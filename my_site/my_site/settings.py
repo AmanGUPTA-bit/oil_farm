e4e4280e9677f283fc55c2b3740932ff7c2f38a3
Aman gupta <aman945492@gmaill.com>
2026-07-11 15:31:27 +0530

    update setting for deployment

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!py=i1yqt0c$uuzz4ir*$+cmx!cbv_xco%ku22i*zc#c&*sw+d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
